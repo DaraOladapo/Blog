e9c8b85a01f170b589836892a74caf70af7808f1
Dara Oladapo <daraoladapo@outlook.com>
2024-07-28 11:44:41 +0100

    ...

---
+++ _posts/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+string directoryPath = "./";
+string[] files = Directory.GetFiles(directoryPath, "*.md");
+
+foreach (string file in files)
+{
+    string content = File.ReadAllText(file);
+    Console.WriteLine($"Content of {file}:\n{content}\n");
+}
